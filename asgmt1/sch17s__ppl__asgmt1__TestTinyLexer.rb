096ce6a29a69430d3aaca4455be332d828d5c10e
hassearle <a.searle274@gmail.com>
2017-01-29 04:58:27 -0800

    SC path set in TTL

--- asgmt1/sch17s__ppl__asgmt1__TestTinyLexer.rb
+++ asgmt1/sch17s__ppl__asgmt1__TestTinyLexer.rb
@@ -1,5 +1,5 @@
-load "/home/asearle/Dropbox/PPL/Asgmt1/sch17s__ppl__asgmt1__TinyToken.rb"
-load "/home/asearle/Dropbox/PPL/Asgmt1/path to sch17s__ppl__asgmt1__TinyScanner.rb"
+load "/home/asearle/dev/asgmt1/sch17s__ppl__asgmt1__TinyToken.rb"
+load "/home/asearle/dev/asgmt1/sch17s__ppl__asgmt1__TinyScanner.rb"
 
 
 # filename.txt below is simply the "source code"
@@ -8,7 +8,7 @@ load "/home/asearle/Dropbox/PPL/Asgmt1/path to sch17s__ppl__asgmt1__TinyScanner.
 # simply include the file name, otherwise, you will need
 # to specify the entire path to the file as we did above
 # to load the other ruby modules
-scan = Scanner.new("filename.txt")
+scan = Scanner.new("sch17s__ppl__asgmt1__SourceCode.txt")
 tok = scan.nextToken()
 while (tok.get_type() != Token::EOF)
 
